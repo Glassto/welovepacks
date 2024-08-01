d436771191539180d4ee2562130f8c525e803c63
Glassto <102603092+Glassto@users.noreply.github.com>
2024-08-01 22:51:30 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,15 @@ function dropdown() {
 }
 
 function displayProducts(maxProductsToDisplay) {
+  let _activeListStyle = document.getElementById("_activeListStyle") || 'List';
       let output = "";
 
-      for (let i = 0; i < maxProductsToDisplay; i++) {
-      let item = products[i];
-        let pricePer = `${item.price} RON <span class="sm:text-base phoneM:text-sm phoneS:text-xs text-gray-500 tracking-wide"> / ${item.per}</span>`
-        output += `
+      if(_activeListStyle.innerText === 'List') {
+
+        for (let i = 0; i < maxProductsToDisplay; i++) {
+          let item = products[i];
+          let pricePer = `${item.price} RON <span class="sm:text-base phoneM:text-sm phoneS:text-xs text-gray-500 tracking-wide"> / ${item.per}</span>`
+          output += `
           <div class="product flex bg-slate-50 border border-solid border-slate-200 hover:border-slate-400/40 hover:shadow-sm rounded-xl p-3">
             <div>
               <img class="sm:min-w-32 sm:h-32 phoneS:min-w-28 phoneS:h-28 w-full rounded-lg" src="${item.image}" alt="${item.image}">
@@ -25,65 +28,16 @@ function displayProducts(maxProductsToDisplay) {
             </div>
           </div>
         `;
-      }
-      document.querySelector(".products-list").innerHTML = output;
-      document.querySelector(".number-product").innerHTML = maxProductsToDisplay;
+        }
+        document.querySelector(".products-list").innerHTML = output;
+        document.querySelector(".number-product").innerHTML = maxProductsToDisplay;
 
-    }
+      } else {
 
-
-
-
-let products = [];
-  let http = new XMLHttpRequest();
-  http.open('get', 'data/produse.json', true);
-  http.send();
-
-  http.onload = function() {
-  document.querySelector(".products-grid").style.display = 'none'
-  document.getElementById('list').style.background = 'white';
-  document.getElementById('grid').style.background = 'transparent';
-    if(this.readyState == 4 && this.status == 200) {
-      products = JSON.parse(this.responseText)
-      displayProducts(10);
-    }
-}
-
-function listItems() {
-  let http = new XMLHttpRequest();
-  http.open('get', 'data/produse.json', true);
-  http.send();
-
-  http.onload = function() {
-  document.querySelector(".products-list").style.display = 'grid'
-  document.querySelector(".products-grid").style.display = 'none'
-  document.getElementById('list').style.background = 'white';
-  document.getElementById('grid').style.background = 'transparent';
-  if(this.readyState == 4 && this.status == 200) {
-    products = JSON.parse(this.responseText)
-    displayProducts(10)
-  }
-}
-}
-
-function gridItems() {
-  let http = new XMLHttpRequest();
-  http.open('get', 'data/produse.json', true);
-  http.send();
-
-  http.onload = function() {
-  document.querySelector(".products-list").style.display = 'none'
-  document.querySelector(".products-grid").style.display = 'grid'
-  document.getElementById('grid').style.background = 'white';
-  document.getElementById('list').style.background = 'transparent';
-  if(this.readyState == 4 && this.status == 200) {
-    let products = JSON.parse(this.responseText)
-
-    let output = "";
-
-    for (let item of products) {
-        let pricePer = `${item.price} RON <span class="text-base sm:text-base phoneM:text-sm phoneS:text-xs text-gray-500 tracking-wide"> / ${item.per}</span>`
-      output += `
+        for (let i = 0; i < maxProductsToDisplay; i++) {
+          let item = products[i];
+          let pricePer = `${item.price} RON <span class="text-base sm:text-base phoneM:text-sm phoneS:text-xs text-gray-500 tracking-wide"> / ${item.per}</span>`
+          output += `
         <div class="product flex flex-col bg-slate-50 border border-solid border-slate-200 hover:border-slate-300 rounded-xl p-3">
           <div>
             <img class="h-full w-full rounded-lg" src="${item.image}" alt="${item.image}">
@@ -100,8 +54,65 @@ function gridItems() {
           </div>
         </div>
       `;
+        }
+        document.querySelector(".products-grid").innerHTML = output;
+        document.querySelector(".number-product").innerHTML = maxProductsToDisplay;
+      }
     }
-    document.querySelector(".products-grid").innerHTML = output;
+
+
+
+let products = [];
+  let http = new XMLHttpRequest();
+  http.open('get', 'data/produse.json', true);
+  http.send();
+
+  http.onload = function() {
+  document.querySelector(".products-grid").style.display = 'none'
+  document.getElementById('list').style.background = 'white';
+  document.getElementById('grid').style.background = 'transparent';
+    if(this.readyState == 4 && this.status == 200) {
+      products = JSON.parse(this.responseText);
+      let _activeListStyle = document.getElementById("_activeListStyle");
+      _activeListStyle.innerHTML = 'List';
+      displayProducts(10);
+    }
+}
+
+function listItems() {
+  let http = new XMLHttpRequest();
+  http.open('get', 'data/produse.json', true);
+  http.send();
+
+  http.onload = function() {
+  document.querySelector(".products-list").style.display = 'grid'
+  document.querySelector(".products-grid").style.display = 'none'
+  document.getElementById('list').style.background = 'white';
+  document.getElementById('grid').style.background = 'transparent';
+  if(this.readyState == 4 && this.status == 200) {
+    products = JSON.parse(this.responseText);
+    let _activeListStyle = document.getElementById("_activeListStyle");
+      _activeListStyle.innerHTML = 'List';
+    displayProducts(10)
+  }
+}
+}
+
+function gridItems() {
+  let http = new XMLHttpRequest();
+  http.open('get', 'data/produse.json', true);
+  http.send();
+
+  http.onload = function() {
+  document.querySelector(".products-list").style.display = 'none'
+  document.querySelector(".products-grid").style.display = 'grid'
+  document.getElementById('grid').style.background = 'white';
+  document.getElementById('list').style.background = 'transparent';
+  if(this.readyState == 4 && this.status == 200) {
+    products = JSON.parse(this.responseText)
+    let _activeListStyle = document.getElementById("_activeListStyle");
+    _activeListStyle.innerHTML = 'Grid';
+    displayProducts(10);
   }
 }
 }
